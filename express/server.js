6fbae30e46a3a6d0e016f6539168bc7ddb04a515
Noé Landré <36192119+InnovantTech@users.noreply.github.com>
2020-03-06 22:21:50 +0100

    Add files via upload

--- express/server.js
+++ express/server.js
@@ -20,7 +20,7 @@ serv.get('/t', (req, res) => {
 
 serv.get('/', async function (req, res) {
     try {
-        console.log(req);
+        console.log(req.body.toString());
         body = req.body;
         console.log("test2");
         var key1 = '%%%InnovanTech%%%hfuhfzeuhehufzeifHUZIUIUAZEGHDRuazsjdczhfzejifjdibhufihezioxdjfusbutfdzae1454rt56aert4aert4aez4rta6traeaertaer%%%InnovanTech%%%';
