8f9b4706ab64ff2c136046025051bec936064729
Noé Landré <36192119+InnovantTech@users.noreply.github.com>
2020-03-06 22:04:48 +0100

    Add files via upload

--- express/server.js
+++ express/server.js
@@ -19,7 +19,7 @@ serv.get('/t', (req, res) => {
 });
 
 serv.get('/', async function (req, res) {
-    try{
+    try {
         body = req.body;
         var key1 = '%%%InnovanTech%%%hfuhfzeuhehufzeifHUZIUIUAZEGHDRuazsjdczhfzejifjdibhufihezioxdjfusbutfdzae1454rt56aert4aert4aez4rta6traeaertaer%%%InnovanTech%%%';
         var key2 = '%%%InnovanTech%%%DZUYGDZYBADJAZZhuiaheajpodkadygufhqsdofjqsdiçfuhjeziu56894518798456489451527845641897edrfjuezfutyzadfbshjfvuyq%%%InnovanTech%%%';
@@ -42,8 +42,8 @@ serv.get('/', async function (req, res) {
             res.send(rtn);
             return true;
         }
-    }catch(e) {
-        res.send("Unknown error");
+    } catch (e) {
+        res.send(e);
         return;
     }
 });
@@ -134,102 +134,102 @@ async function isFirstTime(username, trim) {
 
 
 async function app2(username, password) {
-    try{
-    var json = await get_notes(username, password, false);
-    if (json == "WL") {
-        return "Connexion Error";
-    }
-    json = JSON.parse(json);
-
-    if (json.donneesSec.donnees.listeDevoirs.V[0].periode.V.L) {
-        var trim = parseInt(json.donneesSec.donnees.listeDevoirs.V[0].periode.V.L.replace("Trimestre ", ""));
-    } else {
-        return false;
-    }
-
-    var a = await isFirstTime(username, trim);
-    if (a == "MSE") {
-        return "MySQL Error";
-    }
-
-    if (json.donneesSec.donnees.listeDevoirs.V.length != 0) {
-        var notes = json.donneesSec.donnees.listeDevoirs.V;
-        json.donneesSec.donnees.listeDevoirs.V[0].note.V = 19;
-        json.donneesSec.donnees.listeDevoirs.V[1].note.V = 16;
-        var nb_notes = notes.length;
-        var moy_gen = rmComma(json.donneesSec.donnees.moyGenerale.V);
-        var moy_gen = parseFloat((Math.random() * (20.00 - 0.01) + 0.01).toFixed(2));
-
-        if (a == false) {
-            await pool.query("DELETE FROM `users` WHERE `username` = ?", [username]);
-            await pool.query("DELETE FROM `notes` WHERE `username` = ?", [username]);
-
-            year_start = getYearSchool(1);
-            year_end = getYearSchool(0);
-
-            await pool.query("INSERT INTO `users`(`username`, `nb_notes`, `moyGen`, `trimester`, `year_school_start`, `year_school_end`) VALUES (?,?,?,?,?,?)", [username, nb_notes, moy_gen, trim, year_start, year_end]);
-
-            var i = 0;
-            while (i < nb_notes) {
-                note = rmComma(notes[i].note.V);
-                coef = notes[i].coefficient;
-                scale = notes[i].bareme.V;
-                subject = notes[i].service.V.L;
-                commentary = notes[i].commentaire;
-                date = dateToMySQL(notes[i].date.V);
-
-                await pool.query("INSERT INTO `notes`(`username`, `note`, `coef`, `scale`, `subject`, `commentary`, `date`) VALUES (?,?,?,?,?,?,?)", [username, note, coef, scale, subject, commentary, date]);
-                i++;
-            }
-
-            return ("done");
-        } else {
-            const result = await pool.query("SELECT `moyGen` FROM `users` WHERE `username` = ?", [username]);
-            if (!result[0][0]) {
-                return false;
-            }
-
-            var old_moy = rmComma(result[0][0].moyGen);
-
-            await pool.query("UPDATE `users` SET `nb_notes`= ?,`moyGen`= ? WHERE `username` = ?", [nb_notes, moy_gen, username]);
-            var newNotes = "";
-            var newMoy = "";
-            if (old_moy != moy_gen) {
-                newMoy = '"moy": { "old_moy":' + old_moy + ', "new_moy": ' + moy_gen + '},';
-                newNotes = "{" + newMoy;
-            }
-            newNotes = newNotes + '"new_notes":{"notes":[';
-
-            var i = 0;
-            var nb = 0;
-            while (i < nb_notes) {
-                note = rmComma(notes[i].note.V);
-                coef = notes[i].coefficient;
-                scale = notes[i].bareme.V;
-                subject = notes[i].service.V.L;
-                commentary = notes[i].commentaire;
-                date = dateToMySQL(notes[i].date.V);
-
-                const result = await pool.query("SELECT `id` FROM `notes` WHERE `username` = ? AND `note` = ? AND `coef` = ? AND `scale` = ? AND `subject` = ? AND `commentary` = ? AND date = ?", [username, note, coef, scale, subject, commentary, date]);
-                if (!result[0][0]) {
-                    if (nb != 0) {
-                        newNotes = newNotes + ",";
-                    }
-                    newNotes = newNotes + '{"note": ' + note + ',"coefficient": ' + coef + ',"scale": ' + scale + ',"subject": "' + subject + '", "commentary": "' + commentary + '","date": "' + date + '"}';
-                    await pool.query("INSERT INTO `notes`(`username`, `note`, `coef`, `scale`, `subject`,`commentary`, `date`) VALUES (?,?,?,?,?,?,?)", [username, note, coef, scale, subject, commentary, date]);
-                    nb++;
-                }
-                i++;
-            }
-
-            newNotes = newNotes + '], "nb_notes": ' + nb + '}}';
-            return newNotes;
+    try {
+        var json = await get_notes(username, password, false);
+        if (json == "WL") {
+            return "Connexion Error";
         }
-    } else {
-        return "No Grades";
-    }
-    }catch (e) {
-        return "Unknown Error";
+        json = JSON.parse(json);
+
+        if (json.donneesSec.donnees.listeDevoirs.V[0].periode.V.L) {
+            var trim = parseInt(json.donneesSec.donnees.listeDevoirs.V[0].periode.V.L.replace("Trimestre ", ""));
+        } else {
+            return false;
+        }
+
+        var a = await isFirstTime(username, trim);
+        if (a == "MSE") {
+            return "MySQL Error";
+        }
+
+        if (json.donneesSec.donnees.listeDevoirs.V.length != 0) {
+            var notes = json.donneesSec.donnees.listeDevoirs.V;
+            json.donneesSec.donnees.listeDevoirs.V[0].note.V = 19;
+            json.donneesSec.donnees.listeDevoirs.V[1].note.V = 16;
+            var nb_notes = notes.length;
+            var moy_gen = rmComma(json.donneesSec.donnees.moyGenerale.V);
+            var moy_gen = parseFloat((Math.random() * (20.00 - 0.01) + 0.01).toFixed(2));
+
+            if (a == false) {
+                await pool.query("DELETE FROM `users` WHERE `username` = ?", [username]);
+                await pool.query("DELETE FROM `notes` WHERE `username` = ?", [username]);
+
+                year_start = getYearSchool(1);
+                year_end = getYearSchool(0);
+
+                await pool.query("INSERT INTO `users`(`username`, `nb_notes`, `moyGen`, `trimester`, `year_school_start`, `year_school_end`) VALUES (?,?,?,?,?,?)", [username, nb_notes, moy_gen, trim, year_start, year_end]);
+
+                var i = 0;
+                while (i < nb_notes) {
+                    note = rmComma(notes[i].note.V);
+                    coef = notes[i].coefficient;
+                    scale = notes[i].bareme.V;
+                    subject = notes[i].service.V.L;
+                    commentary = notes[i].commentaire;
+                    date = dateToMySQL(notes[i].date.V);
+
+                    await pool.query("INSERT INTO `notes`(`username`, `note`, `coef`, `scale`, `subject`, `commentary`, `date`) VALUES (?,?,?,?,?,?,?)", [username, note, coef, scale, subject, commentary, date]);
+                    i++;
+                }
+
+                return ("done");
+            } else {
+                const result = await pool.query("SELECT `moyGen` FROM `users` WHERE `username` = ?", [username]);
+                if (!result[0][0]) {
+                    return false;
+                }
+
+                var old_moy = rmComma(result[0][0].moyGen);
+
+                await pool.query("UPDATE `users` SET `nb_notes`= ?,`moyGen`= ? WHERE `username` = ?", [nb_notes, moy_gen, username]);
+                var newNotes = "";
+                var newMoy = "";
+                if (old_moy != moy_gen) {
+                    newMoy = '"moy": { "old_moy":' + old_moy + ', "new_moy": ' + moy_gen + '},';
+                    newNotes = "{" + newMoy;
+                }
+                newNotes = newNotes + '"new_notes":{"notes":[';
+
+                var i = 0;
+                var nb = 0;
+                while (i < nb_notes) {
+                    note = rmComma(notes[i].note.V);
+                    coef = notes[i].coefficient;
+                    scale = notes[i].bareme.V;
+                    subject = notes[i].service.V.L;
+                    commentary = notes[i].commentaire;
+                    date = dateToMySQL(notes[i].date.V);
+
+                    const result = await pool.query("SELECT `id` FROM `notes` WHERE `username` = ? AND `note` = ? AND `coef` = ? AND `scale` = ? AND `subject` = ? AND `commentary` = ? AND date = ?", [username, note, coef, scale, subject, commentary, date]);
+                    if (!result[0][0]) {
+                        if (nb != 0) {
+                            newNotes = newNotes + ",";
+                        }
+                        newNotes = newNotes + '{"note": ' + note + ',"coefficient": ' + coef + ',"scale": ' + scale + ',"subject": "' + subject + '", "commentary": "' + commentary + '","date": "' + date + '"}';
+                        await pool.query("INSERT INTO `notes`(`username`, `note`, `coef`, `scale`, `subject`,`commentary`, `date`) VALUES (?,?,?,?,?,?,?)", [username, note, coef, scale, subject, commentary, date]);
+                        nb++;
+                    }
+                    i++;
+                }
+
+                newNotes = newNotes + '], "nb_notes": ' + nb + '}}';
+                return newNotes;
+            }
+        } else {
+            return "No Grades";
+        }
+    } catch (e) {
+        return e;
     }
 }
 
