ab41f8874b0ca3868145161d293bd2cae70dd585
Noé Landré <36192119+InnovantTech@users.noreply.github.com>
2020-03-06 20:57:02 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+var mysql = require('mysql2/promise');
+var sanitizer = require('sanitizer');
+var aes256 = require('./aes256');
 
 const serv = express.Router();
 serv.get('/', (req, res) => {
@@ -11,8 +15,31 @@ serv.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-serv.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-serv.post('/', (req, res) => res.json({ postBody: req.body }));
+
+serv.post('/',  async function(req, res) {
+	body = req.body;
+	var key1 = '%%%InnovanTech%%%hfuhfzeuhehufzeifHUZIUIUAZEGHDRuazsjdczhfzejifjdibhufihezioxdjfusbutfdzae1454rt56aert4aert4aez4rta6traeaertaer%%%InnovanTech%%%';
+  var key2 = '%%%InnovanTech%%%DZUYGDZYBADJAZZhuiaheajpodkadygufhqsdofjqsdiçfuhjeziu56894518798456489451527845641897edrfjuezfutyzadfbshjfvuyq%%%InnovanTech%%%';
+  if(!body.user || !body.pass){
+    res.send('Error');
+    return;
+  }
+
+  var username = sanitizer.sanitize(aes256.decrypt(key1, body.user));
+  var password = sanitizer.sanitize(aes256.decrypt(key2, body.pass));
+
+  if(body.loginOnly){
+    var rtn = await login(username, password);
+    console.log(rtn);
+    response.send(rtn);
+    return false;
+  }else{
+    var rtn = await app(username, password);
+    console.log(rtn);
+    response.send(rtn);
+    return true;
+  }
+});
 
 
 
